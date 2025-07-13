c83120642444b647568ca4b56008c3d8004346a9
baiching <bachmarma@gmail.com>
2025-07-13 02:24:40 +0200

    66. Plus One

---
+++ src/main/java/com/baiching/array/PlusOne.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+using std::pmr::vector;
+
+vector<int> plusOne(vector<int>& digits) {
+    for (int i = digits.size() - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+        digits[i] = 0;
+    }
+
+    digits.insert(digits.begin(), 1);
+
+    return digits;
+}
